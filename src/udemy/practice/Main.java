c256fd063a4b904c7a72b88553ebce71d4b5af6d
Jonathan Govan <jonathan_govan@homedepot.com>
2020-09-25 08:02:02 -0400

    pushing code up

---
+++ src/udemy/practice/Main.java
@@ -0,0 +1,20 @@
+package udemy.practice;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        printMegaBytesAndKiloBytes(2500);
+
+    }
+
+    public static void printMegaBytesAndKiloBytes(int kiloBytes) {
+        if(kiloBytes < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            int megaBytes = kiloBytes / 1024;
+            int kiloBytesRemainder = kiloBytes % 1024;
+            System.out.println(kiloBytes + " KB = " + megaBytes + " MB and " + kiloBytesRemainder + " KB");
+        }
+    }
+}
